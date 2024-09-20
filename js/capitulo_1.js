70da7795fb5f8a32a1d5d46b98ccf798a093e2a8
sebill77 <114451007+sebill77@users.noreply.github.com>
2024-09-20 08:16:21 -0500

    Add files via upload

---
+++ js/capitulo_1.js
@@ -0,0 +1,23 @@
+var genesisescena2 = document.querySelector('.genesisescena2')
+var oculto = document.querySelector('.oculto')
+var llama = document.querySelector('.llama')
+var nino = document.querySelector('.nino')
+var oculto4 = document.querySelector('.oculto4')
+var sound = document.querySelector('.fuego')
+
+nino.addEventListener("click", Textto)
+llama.addEventListener("click", llamaa)
+genesisescena2.addEventListener("click", animaciones)
+
+
+function Textto() {
+    oculto4.classList.remove('oculto4')
+}
+function llamaa() {
+    llama.classList.add('llamaanicacion')
+    sound.play()
+}
+function animaciones() {
+    genesisescena2.classList.add('genesisanimacion')
+    oculto.classList.remove('oculto')
+}
