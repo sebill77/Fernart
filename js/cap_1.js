8989fddbee9afb03fc4d5a7f77852d7befb3b5b5
sebill77 <svarela8@misena.edu.co>
2024-11-26 14:21:36 -0500

    Primer capitulo interactivo y errores solucionados

--- js/cap_1.js
+++ js/cap_1.js
@@ -27,12 +27,42 @@ var TerceraEscena = document.querySelector('#My-video3')
 var CuartaEscena = document.querySelector('#My-video4')
 var QuintaEscena = document.querySelector('#My-video5')
 var SextaEscena = document.querySelector('#My-video6')
+// otras variables
+var CIRCULO1 = document.querySelector('.circulo1')
+var CIRCULO2 = document.querySelector('.circulo2')
+var CIRCULO3 = document.querySelector('.circulo3')
+var CIRCULO4 = document.querySelector('.circulo4')
+var CIRCULO5 = document.querySelector('.circulo5')
+var CIRCULO6 = document.querySelector('.circulo6')
 
-var CIRCULO = document.querySelector('.circulo')
-
-CIRCULO.addEventListener("click", PrimeraEscena1)
+CIRCULO1.addEventListener("click", PrimeraEscena1)
+CIRCULO2.addEventListener("click", PrimeraEscena2)
+CIRCULO3.addEventListener("click", PrimeraEscena3)
+CIRCULO4.addEventListener("click", PrimeraEscena4)
+CIRCULO5.addEventListener("click", PrimeraEscena5)
+CIRCULO6.addEventListener("click", PrimeraEscena6)
 
 function PrimeraEscena1() {
-    CIRCULO.classList.add('escondericono')
+    CIRCULO1.classList.add('escondericono')
     PrimeraEscena.play()
+}
+function PrimeraEscena2() {
+    CIRCULO2.classList.add('escondericono')
+    SegundaEscena.play()
+}
+function PrimeraEscena3() {
+    CIRCULO3.classList.add('escondericono')
+    TerceraEscena.play()
+}
+function PrimeraEscena4() {
+    CIRCULO4.classList.add('escondericono')
+    CuartaEscena.play()
+}
+function PrimeraEscena5() {
+    CIRCULO5.classList.add('escondericono')
+    QuintaEscena.play()
+}
+function PrimeraEscena6() {
+    CIRCULO6.classList.add('escondericono')
+    SextaEscena.play()
 }
